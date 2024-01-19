a38b200aa35c11088864f08c47ef96f5a8860419
Yukun Xie <yxie@lcs.on.ca>
2024-01-19 08:57:56 -0500

    Initial Commit

---
+++ TradingCards/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TradingCards
+//
+//  Created by Yukun Xie on 2024/1/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Yukun Xie on 2024/1/19.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
